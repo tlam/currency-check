c581c5a345b86ad30913fc9fa1f750a5dbcf90c5
Thierry <lamthierry@gmail.com>
2016-02-14 14:45:32 -0500

    Adding basic form submission

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,21 +4,46 @@ import javax.inject.Inject
 import scala.concurrent.Future
 
 import play.api._
+import play.api.data._
+import play.api.data.Forms._
+import play.api.data.format.Formats._ 
 import play.api.mvc._
-import play.api.libs.json.Json
+import play.api.libs.json._
 import play.api.libs.ws._
+import play.api.i18n._
 
-class Application @Inject() (ws: WSClient) extends Controller {
+case class CurrencyData(eur: Double, usd: Double)
+case class Currency(base: String, date: String)
+
+class Application @Inject() (val messagesApi: MessagesApi, ws: WSClient) extends Controller with I18nSupport {
+  val currencyForm = Form(mapping(
+      "eur" -> of[Double],
+      "usd" -> of[Double]
+    )(CurrencyData.apply)(CurrencyData.unapply))
+
+  def currencyPost = Action { implicit request => 
+    currencyForm.bindFromRequest().fold(
+      formWithErrors => BadRequest(views.html.index(formWithErrors)),
+      currency => Ok("Calculated"))
+  }
 
   def index = Action {
-    var greet = "Hello"
-    //Ok(views.html.index("Your new application is ready."))
-    Ok(views.html.index(greet))
+    Ok(views.html.index(currencyForm))
   }
 
   def getCurrencies = Action.async { request =>
     implicit val context = play.api.libs.concurrent.Execution.Implicits.defaultContext
     val request: WSRequest = ws.url("http://api.fixer.io/2014-12-12?base=CAD")
+    implicit val currencyReads = Json.reads[Currency]
+    val futureResult: Future[JsResult[Currency]] = request.get().map {
+      response => (response.json).validate[Currency]
+    }
+    val response: Future[WSResponse] = request.get()
+    var other = response.map {
+      response => response.json
+    }
+    val baseReads: Reads[String] = (JsPath \ "base").read[String]
+    //val baseResult: JsResult[String] = response.validate[String](baseReads)
     val futureResponse: Future[WSResponse] = request.get()
     futureResponse.map{
       jsResponse => Ok(jsResponse.body).as("application/json")
